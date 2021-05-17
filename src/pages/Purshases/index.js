d1a52bef37448437d3def042a55021b6fa0362c3
Carlos Lopes <dev.carloslopes@gmail.com>
2021-05-17 16:55:14 -0300

    feat: create page of purshases

--- src/pages/Purshases/index.js
+++ src/pages/Purshases/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useEffect, useState } from 'react';
+import formatCurrency from '../../utils/formatCurrency';
+import PurshasesContainer from './styles';
 
 function Purshases() {
+  const [purshasedProducts, setPurshasedProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const getPurshasedProducts = localStorage.getItem('purshases');
+      if (getPurshasedProducts) {
+        const formatPurshasedProducts = await JSON.parse(getPurshasedProducts);
+
+        setPurshasedProducts(formatPurshasedProducts);
+      }
+    })();
+  }, []);
+
   return (
-    <div>
-      <h1>Compras</h1>
-    </div>
+    <PurshasesContainer>
+      <table className="table">
+        <thead className="table__header">
+          <tr>
+            <th>Pedido</th>
+            <th>Comprador</th>
+            <th>Valor do Pedido</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!purshasedProducts ? (
+            <span>NADA</span>
+          ) : (
+            purshasedProducts.map((item, id) => (
+              <tr className="table__body" key={id}>
+                <td># {id}</td>
+                <td>{item.name}</td>
+                <td>{formatCurrency(item.totalValueOfProducts)}</td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+    </PurshasesContainer>
   );
 }
 
