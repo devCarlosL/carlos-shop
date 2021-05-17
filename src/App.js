42ca2cfcb1a7387a18551ecbd52413e057d24181
Carlos Lopes <dev.carloslopes@gmail.com>
2021-05-17 15:53:19 -0300

    feat: add navigation provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import { NavigationProvider } from './contexts/navigation';
+
+import routes from './routes';
+
+import GlobalStyles from './assets/styles/GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Início</h1>
-    </div>
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route exact key={path} path={path} component={component} />
+      ))}
+      <Redirect to="/home" />
+    </Switch>
   );
 }
 
-export default App;
+export default function () {
+  return (
+    <Router>
+      <NavigationProvider>
+        <GlobalStyles />
+        <App />
+      </NavigationProvider>
+    </Router>
+  );
+}
