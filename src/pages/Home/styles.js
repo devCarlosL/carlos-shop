67e961729518176fa000a1ac49fb0d6a16bca690
Carlos Lopes <dev.carloslopes@gmail.com>
2021-05-17 16:40:29 -0300

    feat: add styles from home page

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled('div')`
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+  }
+
+  ul li {
+    flex: 0 auto;
+  }
+`;
+
+export default HomeContainer;
