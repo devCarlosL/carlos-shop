b0bc3e57e978c7e3044a889136e7c319fbe824eb
Carlos Lopes <dev.carloslopes@gmail.com>
2021-05-17 16:41:26 -0300

    feat: create home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useEffect, useState } from 'react';
+
+import HomeCard from '../../components/HomeCard';
+
+import { useCart } from '../../contexts/cart';
+
+import HomeContainer from './styles';
 
 function Home() {
+  const { handleAddItemToCart } = useCart();
+  const [storedProducts, setStoredProducts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const getStoredProducts = localStorage.getItem('products');
+      if (getStoredProducts) {
+        const storedProductsFormat = await JSON.parse(getStoredProducts);
+
+        setStoredProducts(storedProductsFormat);
+      }
+    })();
+  }, []);
+
+  console.log(storedProducts);
+
   return (
-    <div>
-      <h1>Início</h1>
-    </div>
+    <HomeContainer>
+      <ul>
+        {storedProducts.map((item, i) => (
+          <li key={i}>
+            <HomeCard
+              product={item.product}
+              price={item.price}
+              textButton="Adicionar"
+              onClick={() =>
+                handleAddItemToCart(
+                  i,
+                  item.product,
+                  item.description,
+                  item.price,
+                  item.quantity,
+                )
+              }
+            />
+          </li>
+        ))}
+      </ul>
+    </HomeContainer>
   );
 }
 
